4bbef4cfc248759fe730c96a4b7c4454ab9638c5
Rakesh Mistry <mistryrn@mcmaster.ca>
2014-12-29 21:38:24 -0500

    Added 'Archive' tab to Tour page, cleaned up Tour CSS

--- wp-content/themes/aukland/tour.php
+++ wp-content/themes/aukland/tour.php
@@ -6,25 +6,84 @@
 
 get_header(); ?>
 
-<div id="main-content" class="row" data-equalizer><!-- Foundation .row start -->
+<div id="main-content" class="row"><!-- Foundation .row start -->
 
-    <div class="small-12 columns" data-equalizer-watch><!-- Foundation .columns start -->
+    <div class="small-12 columns"><!-- Foundation .columns start -->
 
       <div id="primary" class="content-area">
         <main id="main" class="site-main" role="main">
 
             <h3>TOUR</h3>
-
-            <?php
-            $response = file_get_contents("http://api.bandsintown.com/artists/Aukland/events.json?api_version=2.0&app_id=AUKLANDSITE");
-            $response = json_decode($response);
-            if ($response != null) {
+            <dl class="tabs" data-tab>
+              <dd class="active"><a href="#upcoming-tab"><h4>Upcoming</h4></a></dd>
+              <dd><a href="#archive-tab"><h4>Archive</h4></a></dd>
+            </dl>
+            <div class="tabs-content">
+              <div class="content active" id="upcoming-tab">
+                <?php
+                $response = file_get_contents("http://api.bandsintown.com/artists/Aukland/events.json?api_version=2.0&app_id=AUKLANDSITE");
+                $response = json_decode($response);
+                if ($response != null) {
+                    $i = 0;
+                    foreach ($response as $event) {
+                      echo "<div class='tour-date small-12 columns' id='date-$i'>";
+                      $datetime=$event->datetime;
+                      $time=strtotime($datetime);
+                      $showdate = date("M jS",$time);
+                      $showday = date("D",$time);
+                      $venue = $event->venue->name;
+                      $city = $event->venue->city;
+                      if ($event->venue->region == '08') {
+                          $region = "ON";
+                      } else {            
+                          $region = $event->venue->region;
+                      }
+                      // Date-Time Info
+                        echo "<div class='small-3 medium-2 columns no-pad'>";
+                          echo "<div class='small-12 columns' id='date'>";
+                            echo "<h4>$showdate</h4> "; // rsvp
+                          echo "</div>";
+                        echo "</div>";
+                      // Location Info
+                        echo "<div class='small-7 medium-8 large-8 columns no-pad top-pad' id='location'>";
+                          echo "<div class='small-12 medium-7 large-8 columns no-pad'>";
+                            echo "<h4>$venue</h4>";
+                          echo "</div>";
+                          echo "<div class='small-12 medium-5 large-4 columns no-pad'>";
+                            echo "<h4>$city, $region</h4>";
+                          echo "</div>";
+                        echo "</div>";
+                      // Actions
+                        echo "<div class='small-2 medium-2 large-2 columns no-pad' id='actions'>";
+                            echo "<a href='$event->facebook_rsvp_url' id='rsvp'>RSVP</a> "; // rsvp
+                            if ($event->ticket_url) {
+                                echo "<a href='$event->ticket_url'>Tickets</a>"; // tickets
+                            } else {
+                                echo "<a href='' class='inactive'>Tickets</a>"; // tickets (none)
+                            }
+                        echo "</div>";
+                      echo "</div>";
+                      $i++;
+                    }
+                } else {
+                    echo "<div class='tour-date small-12 columns'>";
+                      echo "<h4>Sorry, there are no upcoming shows at this time. Please check back soon!</h4>";
+                      echo "<p>Sign up below and get notified whenever Aukland comes to town:</p>";
+                      echo "<a href='http://www.bandsintown.com/Aukland/start_tracking?came_from=AUKLANDSITE' class='button small success'>Notify Me</a>";
+                    echo "</div>";
+                }
+                ?>
+              </div>
+              <div class="content" id="archive-tab">
+                <?php
+                $response = file_get_contents("http://api.bandsintown.com/artists/Aukland/events.json?api_version=2.0&date=all&app_id=AUKLANDSITE");
+                $response = json_decode($response);
                 $i = 0;
                 foreach ($response as $event) {
                   echo "<div class='tour-date small-12 columns' id='date-$i'>";
                   $datetime=$event->datetime;
                   $time=strtotime($datetime);
-                  $showdate = date("M jS",$time);
+                  $showdate = date("M jS, Y",$time);
                   $showday = date("D",$time);
                   $venue = $event->venue->name;
                   $city = $event->venue->city;
@@ -34,44 +93,26 @@ get_header(); ?>
                       $region = $event->venue->region;
                   }
                   // Date-Time Info
-                    echo "<div class='small-3 medium-2 columns no-pad'>";
-                      echo "<div class='small-12 columns no-pad' id='date'>";
+                    echo "<div class='small-5 medium-4 columns no-pad'>";
+                      echo "<div class='small-12 columns' id='date'>";
                         echo "<h4>$showdate</h4> "; // rsvp
                       echo "</div>";
-                      // echo "<div class='small-12 columns no-pad' id='day'>";
-                      //   echo "<p>$showday</p> "; // rsvp
-                      // echo "</div>";
                     echo "</div>";
                   // Location Info
-                    echo "<div class='small-7 medium-8 large-8 columns no-pad' id='location'>";
-                      echo "<div class='small-12 medium-7 large-8 columns no-pad'>";
+                    echo "<div class='small-7 medium-8 columns no-pad' id='location'>";
+                      echo "<div class='small-12 medium-7 large-8 columns'>";
                         echo "<h4>$venue</h4>";
                       echo "</div>";
-                      echo "<div class='small-12 medium-5 large-4 columns no-pad'>";
+                      echo "<div class='small-12 medium-5 large-4 columns'>";
                         echo "<h4>$city, $region</h4>";
                       echo "</div>";
-                    echo "</div>";
-                  // Actions
-                    echo "<div class='small-2 medium-2 large-2 columns no-pad' id='actions'>";
-                        echo "<a href='$event->facebook_rsvp_url' id='rsvp'>RSVP</a> "; // rsvp
-                        if ($event->ticket_url) {
-                            echo "<a href='$event->ticket_url'>Tickets</a>"; // tickets
-                        } else {
-                            echo "<a href='' class='inactive'>Tickets</a>"; // tickets (none)
-                        }
                     echo "</div>";
                   echo "</div>";
                   $i++;
                 }
-            } else {
-                echo "<div class='tour-date small-12 columns'>";
-                  echo "<h4>Sorry, there are no upcoming shows at this time. Please check back soon!</h4>";
-                  echo "<p>Sign up below and get notified whenever Aukland comes to town:</p>";
-                  echo "<a href='http://www.bandsintown.com/Aukland/start_tracking?came_from=AUKLANDSITE' class='button small success'>Notify Me</a>";
-                echo "</div>";
-            }
-            ?>            
-            
+                ?>
+              </div>
+            </div>
         </main><!-- #main -->
       </div><!-- #primary -->
 
