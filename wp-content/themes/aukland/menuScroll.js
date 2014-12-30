2a8ca594cbfe00b9bc23b199368a8c31c96ea03e
Rakesh Mistry <mistryrn@mcmaster.ca>
2014-12-30 04:42:28 -0500

    Fixed bugs in menuScroll.js, it now recalculates on window resize and orientation change.

--- wp-content/themes/aukland/menuScroll.js
+++ wp-content/themes/aukland/menuScroll.js
@@ -1,24 +1,41 @@
-if(! jQuery('.nav').hasClass('regular') ) {
-// get the value of the bottom of the #aukland element by adding the offset of that element plus its height, set it as a variable
-var maintop = jQuery('#aukland').offset().top - 18;
+jQuery(window).load(function() {
+    // Only perform menuScroll calculations on home page
+    if(! jQuery('.nav').hasClass('regular') ) {
+        // Get position of top of #aukland element, set as 'maintop'
+        var maintop = jQuery('#aukland').offset().top - 21;
+        console.log('used ' + maintop);
 
-// on scroll
-jQuery(window).on('scroll gesturechange touchmove', function() {
+        jQuery(window).on('scroll gesturechange touchmove', function() {
+            stop = Math.round(jQuery(window).scrollTop());
+            if (stop > maintop) {
+                jQuery('.nav').addClass('past-aukland');
+                jQuery('.menu-logo').show();
+                jQuery('#mobile-nav').show();
+            } else {
+                jQuery('.nav').removeClass('past-aukland');
+                jQuery('.menu-logo').hide();
+                jQuery('#mobile-nav').hide();
+            }
+        });
 
-    // round here to reduce some workload
-    stop = Math.round(jQuery(window).scrollTop());
-    if (stop > maintop) {
+        var delta;
+        // Update 'maintop' on window resize or orientation change
+        function updateVars() {
+            maintop = jQuery('#aukland').offset().top - 21;
+            console.log('detected! should be ' + maintop);
+        }
+        // Detect window resize or orientation change
+        jQuery(window).on('resize orientationchange', function() {
+            // Using timeouts to only fire updateVars when the page is ready
+            clearTimeout(delta);
+            delta = setTimeout(function(){
+                updateVars();
+            }, 300);
+        });
+
+    } else {
         jQuery('.nav').addClass('past-aukland');
         jQuery('.menu-logo').show();
-        jQuery('#mobile-nav').show();
-    } else {
-        jQuery('.nav').removeClass('past-aukland');
-        jQuery('.menu-logo').hide();
-        jQuery('#mobile-nav').hide();
+        jQuery('#mobile-nav').show();    
     }
 });
-} else {
-    jQuery('.nav').addClass('past-aukland');
-    jQuery('.menu-logo').show();
-    jQuery('#mobile-nav').show();    
-}
